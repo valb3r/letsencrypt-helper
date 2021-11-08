c04d4bed2135c939637fb90572b391a710c3db17
valb3r <valb3r.ua@gmail.com>
2021-11-08 16:02:35 +0200

    Add short note

--- example/src/main/java/example/rest/ExampleRestController.java
+++ example/src/main/java/example/rest/ExampleRestController.java
@@ -3,6 +3,7 @@ package example.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Just and example RestController, so that project is not so empty
 @RestController
 public class ExampleRestController {
 
