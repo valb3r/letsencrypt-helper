793862140aa7c3691c7ea0f4af705ea79708ae7f
valb3r <valb3r.ua@gmail.com>
2021-11-09 23:06:25 +0200

    More tests

--- src/test/java/com/github/valb3r/letsencrypt/BaseTest.java
+++ src/test/java/com/github/valb3r/letsencrypt/BaseTest.java
@@ -2,62 +2,52 @@ package com.github.valb3r.letsencrypt;
 
 import com.github.valb3r.letsencrypt.dummyapp.DummyApp;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.Testcontainers;
-import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.cert.X509Certificate;
 import java.time.Duration;
 
 import static com.github.valb3r.letsencrypt.HttpUtil.helloUrl;
 import static com.github.valb3r.letsencrypt.HttpUtil.httpGet;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
-@ActiveProfiles("test-missing-keystore")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = DummyApp.class)
-class TomcatWellKnownLetsEncryptChallengeEndpointConfigPebbleTest {
+abstract public class BaseTest {
 
     public static final Duration TIMEOUT = Duration.ofMinutes(1);
 
     public static GenericContainer<?> PEBBLE;
     public static GenericContainer<?> PEBBLE_CHALL;
 
+    @BeforeEach
+    public void init() throws IOException {
+        Files.copy(Paths.get("src/test/resources/template-expired-test-keystore"), Paths.get("src/test/resources/expired-test-keystore"), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(Paths.get("src/test/resources/template-not-expired-test-keystore"), Paths.get("src/test/resources/not-expired-test-keystore"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
     @AfterEach
     public void stop() {
         PEBBLE.stop();
         PEBBLE_CHALL.stop();
-        new File("test-keystore").delete();
+        new File("temp-test-keystore").delete();
     }
 
-    @Test
-    void testWhenNoKeystoreNewCertificateGranted() {
-        assertThat(callHelloAndGetIssuerDn()).isEqualTo("CN=letsencrypt-java-helper");
-        launchPebbleContainers();
-        await().atMost(TIMEOUT).until(() -> callHelloAndGetIssuerDn().contains("Pebble"));
-        assertThat(callHelloAndGetIssuerDn()).startsWith("CN=Pebble Intermediate CA");
-    }
-
-    @Test
-    void testWhenExpiredCertificateNewGranted() {
-    }
-
-    @Test
-    void testWhenValidNoNewCertificateGranted() {
-    }
-
-    private String callHelloAndGetIssuerDn() {
+    protected String callHelloAndGetIssuerDn() {
         var certs = httpGet(helloUrl(), "hello");
         assertThat(certs).hasSize(1);
         return ((X509Certificate) certs[0]).getIssuerDN().getName();
     }
 
-    private static void launchPebbleContainers() {
+     static void launchPebbleContainers() {
         Testcontainers.exposeHostPorts(5002);
         PEBBLE = new FixedHostPortGenericContainer<>("letsencrypt/pebble")
                 .withCommand("pebble -config /test/config/pebble-config.json -strict")
