c1fe386fb189fbc1b530c7969f496cd3102b137c
valb3r <valb3r.ua@gmail.com>
2023-03-25 15:02:22 +0200

    Initial work on Spring 3.x support (Jetty has issues)

--- tomcat-spring-boot-2.5.6/src/test/java/com/github/valb3r/letsencrypthelper/tomcat/TomcatNoKeystorePebbleTest.java
+++
@@ -1,6 +0,0 @@
-package com.github.valb3r.letsencrypthelper.tomcat;
-
-import com.github.valb3r.letsencrypthelper.NoKeystorePebbleTest;
-
-class TomcatNoKeystorePebbleTest extends NoKeystorePebbleTest {
-}
